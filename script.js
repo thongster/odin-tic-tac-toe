305dadb100eba4e268aa6d9515903fba3298ce4a
thongster <106802301+thongster@users.noreply.github.com>
2025-10-23 16:06:05 +0700

    winning combos object, push player selection to array, function to compare arrays and determine winner

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-const gameBoard = [
-    {topLeft: false},
-    {topMiddle: false},
-    {topRight: false},
-    {middleLeft: false},
-    {middleMiddle: false},
-    {middleRight: false},
-    {bottomLeft: false},
-    {bottomMiddle: false},
-    {bottomRight: false}
-];
+const gameBoard = {
+comboOne: ["topLeft", "topMiddle", "topRight"],
+comboTwo: ["middleLeft", "middleMiddle", "middleRight"],
+comboThree: ["bottomLeft", "bottomMiddle", "bottomRight"],
+comboFour: ["topLeft", "middleLeft", "bottomLeft"],
+comboFive: ["topMiddle", "middleMiddle", "bottomMiddle"],
+comboSix: ["topRight", "middleRight", "bottomRight"],
+comboSeven: ["topLeft", "middleMiddle", "bottomRight"],
+comboEight: ["topRight", "middleMiddle", "bottomLeft"],
+};
 
 function createPlayer(name) {
     let score = 0;
     let selection = [];
-
     const getScore = () => score;
-    const addScore = () => score++;
+    const addScore = () => score = score + 1;
     const getSelection = () => selection;
-    
-    const addSelection = (choice) => {
+    const addSelection = function(choice) {
         selection.push(choice);
     };
-    return { name, getScore, addScore, getSelection, addSelection };
+    return {name, getScore, addScore, getSelection, addSelection};
 };
 
 const playerOne = createPlayer("Toan");
 const playerTwo = createPlayer("Tuyen");
 
-console.log(playerOne)
-console.log(playerTwo)
-
 const gameFlow = function() {
 
+    function playGame() {
+        playerOne.addSelection(prompt("Player 1, 1st choice: "));
+        playerTwo.addSelection(prompt("Player 2, 1st choice: "));
+    }
+
+    function contains(player, combo) {
+        return combo.every((num) => player.getSelection().includes(num));
+    };
+
+    return {playGame, contains}
 };
 
+// for "every" element in the selected combo, check if it includes the player selection
+for (let combo in gameBoard) {
+    if (gameFlow().contains(playerOne, gameBoard[combo])) {
+        console.log("Player One wins");
+    } else if ((gameFlow().contains(playerTwo, gameBoard[combo]))) {
+        console.log("Player Two wins");
+    };
+};
